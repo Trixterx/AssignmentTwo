b49d6d78d693bc7762dbfafe1b1103e0b4a3ab1c
Dennis Lindquist <dennislindquist@gmail.com>
2020-12-14 20:53:16 +0100

    123456

--- AssignmentTwo/Program.cs
+++ AssignmentTwo/Program.cs
@@ -1,29 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Diagnostics.Tracing;
 
 namespace AssignmentTwo
 {
 
-    // Pinkod vid start, namn på bassgrupp == password. Om fel kod == felmeddelande
-    // Meny med olika val
-    // Lista alla deltagare och använd komma , som separatör
-    // 10 detaljer om varje medlem så som fav mat eller band
+    // Pinkod vid start, namn på bassgrupp == password. Om fel kod == felmeddelande.
+    // Meny med olika val.
+    // Lista alla deltagare och använd komma , som separatör.
+    // 10 detaljer om varje medlem så som fav mat eller band.
     // Största driv till programmering.
-    // Möjlighet att ta bort en person
-
+    // Möjlighet att ta bort en person.
+    
     class ClassMate
     {
-        string name;
-        int age;
-        int length;
-        string adress;
-        string hobby;
-        string favFood;
-        string favDrink;
-        string favBand;
-        int nrChildren;
-        string programmingDrive;
+        private string name;
+        private int age;
+        private int length;
+        private string adress;
+        private string hobby;
+        private string favFood;
+        private string favDrink;
+        private string favBand;
+        private int nrChildren;
+        private string programmingDrive;
 
         public ClassMate()
         {
@@ -54,12 +55,11 @@ namespace AssignmentTwo
         public string FavBand { get => favBand; set => favBand = value; }
         public int NrChildren { get => nrChildren; set => nrChildren = value; }
         public string ProgrammingDrive { get => programmingDrive; set => programmingDrive = value; }
-
     }
 
     class Program
     {
-        static List<ClassMate> listOfClassMates = new List<ClassMate>();
+        private static List<ClassMate> listOfClassMates = new List<ClassMate>();
 
         static void Main(string[] args)
         {
@@ -136,55 +136,48 @@ namespace AssignmentTwo
         }
         private static void DisplayMemberDetails()
         {
+            int counter = 1;
             foreach (var classMate in listOfClassMates)
             {
-                Console.Write($"Name: {classMate.Name} Age: {classMate.Age} Length: {classMate.Length} Adress: {classMate.Adress} Hobby: {classMate.Hobby} Favorite Food: {classMate.FavFood} Favorite Drink: {classMate.FavDrink} Favorite Band: {classMate.FavBand} Children: {classMate.NrChildren}");
+                Console.Write($"{counter}.{classMate.Name}\n  Age: {classMate.Age}\n  Length: {classMate.Length}\n  Adress: {classMate.Adress}\n  Hobby: {classMate.Hobby}\n  Fav Food: {classMate.FavFood}\n  Fav Drink: {classMate.FavDrink}\n  Fav Band: {classMate.FavBand}\n  Children: {classMate.NrChildren}\n");
+                counter++;
             }
-        }6
+            Menu();
+        }
         private static void DisplayMembersDriveToProgramming()
         {
+            int counter = 1;
             foreach (var classMate in listOfClassMates)
             {
-               Console.WriteLine($"{classMate.Name}, Programmerings driv: {classMate.ProgrammingDrive}");
+               Console.WriteLine($"{counter}.{classMate.Name}\n  Programmerings driv: {classMate.ProgrammingDrive}");
+                counter++;
             }
             Menu();
         }
         private static void RemoveMember()
         {
-            string input = "";
+            int input;
             Console.WriteLine("Who do you wanna remove?");
+            input = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < listOfClassMates.Count; i++)
             {
-                Console.WriteLine($"{i+1}. {listOfClassMates[i].Name}");
+               Console.WriteLine($"{i + 1}. {listOfClassMates[i].Name}");
             }
-            input = Console.ReadLine();
+            listOfClassMates.RemoveAt(input-1);
             Menu();
         }
-        static void CreateMembers()
+        private static void CreateMembers()
         {
-            ClassMate DennisLindquist = new ClassMate("Dennis Lindquist", 32, 182, "Älvdalen", "Musik/Gitarr", "Friterad kyckling", "Öl", "Metallica", 1, "Att får skapa och kunna vara kreativ. Men även att få göra ett byte av karriär.");
-            ClassMate FredrikHoffman = new ClassMate("Fredrik Hoffman", 28, 192, "Odensala", "Musik, socialisera, film", "Entrecote med rotfrukter och vitlökssmör", "Trocadero Zero", "Armin Van Buuren", 0, "Möjlighet till karriärutveckling");
-            ClassMate BennyChristensen = new ClassMate("Benny Christensen", 38, 194, "Brunflo", "Datorspel, fiske, programmering, landsvägscykling", "Älgkebab", "Coca Cola", "Foo Fighters", 2, "Att kunna skapa något från grunden och lösa problem med det jag skapat. Att sedan kunna använda detta till att tjäna hutlösa summor pengar är ju i sig ytterligare en morot");
-            ClassMate HakanEriksson = new ClassMate("Håkan Eriksson", 44, 187, "Uppsala", "Moto-X, sporthojar, online gaming", "Shish kebab", "Loka Citron", "Disturbed", 0, "Social engineering, datasäkerhet, pentesting.");
-            ClassMate TinaEriksson = new ClassMate("Tina Eriksson", 30, 165, "Brunflo", "Spela TV-spel", "Potatis & purjolöksoppa", "Kaffe", "Avicii", 2, "Få ett bra jobb, som jag tycker om.");
-            ClassMate TobiasBinett = new ClassMate("Tobias Binett", 31, 192, "Hudiksvall", "Träning, musik, spel, familjen", "Kött", "Öl", "The Black Dahlia Murder", 2, "Att kunna skapa något användbart för mig själv och andra och att ha möjligheten att arbeta med det.");
-            ClassMate EmilOrjes = new ClassMate("Emil Örjes", 26, 184, "Falun", "Snowboard, gitarr, musik, hunden, TV-spel", "Feta hamburgare", "Öl", "System of a Down", 0, "Att lära sig ett nytt yrke helt från grunden som känns givande.");
-            ClassMate MattiasVikstrom = new ClassMate("Mattias Vikström", 33, 187, "Umeå", "Fiske, matlagning", "Cowboysoppa", "Gin och Tonic", "Infected Mushroom", 0, "Personlig utveckling och kreativitet.");
-            ClassMate NicklasEriksson = new ClassMate("Nicklas Eriksson", 26, 175, "Umeå", "Skidor, cykel, simma, springa, fjällvandring, klättring och dataspel", "Gröt med jordnötssmör", "Whiskey", "Falling in Reverse och Self Deception", 0, "Drivet kommer från att man får vara kreativ och en problemlösare på samma gång. Sen så drivs man såklart av att få testa på en annan karriär än den man har haft tidigare.");
-            ClassMate JosefineRonnqvist = new ClassMate("Josefine Rönnqvist", 34, 164, "Gideå", "Sy, pussla, umgås", "Frukt", "Vatten", "Halsbandet", 2, "Personlig utveckling och karriärbyte.");
-            listOfClassMates.Add(DennisLindquist);
-            listOfClassMates.Add(FredrikHoffman);
-            listOfClassMates.Add(BennyChristensen);
-            listOfClassMates.Add(HakanEriksson);
-            listOfClassMates.Add(TinaEriksson);
-            listOfClassMates.Add(TobiasBinett);
-            listOfClassMates.Add(EmilOrjes);
-            listOfClassMates.Add(MattiasVikstrom);
-            listOfClassMates.Add(NicklasEriksson);
-            listOfClassMates.Add(JosefineRonnqvist);
-
-
-
+            listOfClassMates.Add(new ClassMate { Name = "Dennis Lindquist", Age = 32, Length = 182, Adress = "Älvdalen", Hobby = "Musik/Gitarr", FavFood = "Friterad kyckling", FavDrink = "Öl", FavBand = "Metallica", NrChildren = 1, ProgrammingDrive = "Att får skapa och kunna vara kreativ. Men även att få göra ett byte av karriär." });
+            listOfClassMates.Add(new ClassMate { Name = "Fredrik Hoffman", Age = 28, Length = 192, Adress = "Odensala", Hobby = "Musik, socialisera, film", FavFood = "Entrecote med rotfrukter och vitlökssmör", FavDrink = "Trocadero Zero", FavBand = "Armin Van Buuren", NrChildren = 0, ProgrammingDrive = "Möjlighet till karriärutveckling" });
+            listOfClassMates.Add(new ClassMate { Name = "Benny Christensen", Age = 38, Length = 194, Adress = "Brunflo", Hobby = "Datorspel, fiske, programmering, landsvägscykling", FavFood = "Älgkebab", FavDrink = "Coca Cola", FavBand = "Foo Fighters", NrChildren = 2, ProgrammingDrive = "Att kunna skapa något från grunden och lösa problem med det jag skapat. Att sedan kunna använda detta till att tjäna hutlösa summor pengar är ju i sig ytterligare en morot" });
+            listOfClassMates.Add(new ClassMate { Name = "Håkan Eriksson", Age = 44, Length = 187, Adress = "Uppsala", Hobby = "Moto-X, sporthojar, online gaming", FavFood = "Shish kebab", FavDrink = "Loka Citron", FavBand = "Disturbed", NrChildren = 0, ProgrammingDrive = "Social engineering, datasäkerhet, pentesting." });
+            listOfClassMates.Add(new ClassMate { Name = "Tina Eriksson", Age = 30, Length = 165, Adress = "Brunflo", Hobby = "Spela TV-spel", FavFood = "Potatis & purjolöksoppa", FavDrink = "Kaffe", FavBand = "Avicii", NrChildren = 2, ProgrammingDrive = "Få ett bra jobb, som jag tycker om." });
+            listOfClassMates.Add(new ClassMate { Name = "Tobias Binett", Age = 31, Length = 192, Adress = "Hudiksvall", Hobby = "Träning, musik, spel, familjen", FavFood = "Kött", FavDrink = "Öl", FavBand = "The Black Dahlia Murder", NrChildren = 2, ProgrammingDrive = "Att kunna skapa något användbart för mig själv och andra och att ha möjligheten att arbeta med det." });
+            listOfClassMates.Add(new ClassMate { Name = "Emil Örjes", Age = 26, Length = 184, Adress = "Falun", Hobby = "Snowboard, gitarr, musik, hunden, TV-spel", FavFood = "Feta hamburgare", FavDrink = "Öl", FavBand = "System of a Down", NrChildren = 0, ProgrammingDrive = "Att lära sig ett nytt yrke helt från grunden som känns givande." });
+            listOfClassMates.Add(new ClassMate { Name = "Mattias Vikström", Age = 33, Length = 187, Adress = "Umeå", Hobby = "Fiske, matlagning", FavFood = "Cowboysoppa", FavDrink = "Gin och Tonic", FavBand = "Infected Mushroom", NrChildren = 0, ProgrammingDrive = "Personlig utveckling och kreativitet." });
+            listOfClassMates.Add(new ClassMate { Name = "Nicklas Eriksson", Age = 26, Length = 175, Adress = "Umeå", Hobby = "Skidor, cykel, simma, springa, fjällvandring, klättring och dataspel", FavFood = "Gröt med jordnötssmör", FavDrink = "Whiskey", FavBand = "Falling in Reverse och Self Deception", NrChildren = 0, ProgrammingDrive = "Drivet kommer från att man får vara kreativ och en problemlösare på samma gång. Sen så drivs man såklart av att få testa på en annan karriär än den man har haft tidigare." });
+            listOfClassMates.Add(new ClassMate { Name = "Josefine Rönnqvist", Age = 34, Length = 164, Adress = "Gideå", Hobby = "Sy, pussla, umgås", FavFood = "Frukt", FavDrink = "Vatten", FavBand = "Halsbandet", NrChildren = 2, ProgrammingDrive = "Personlig utveckling och karriärbyte." });
         }
     }
 }
