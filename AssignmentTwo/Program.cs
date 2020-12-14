5714eec9eae5c9a77e2b4ac5684ae1f73790169f
Dennis Lindquist <dennislindquist@gmail.com>
2020-12-14 02:27:31 +0100

    New changes

--- AssignmentTwo/Program.cs
+++ AssignmentTwo/Program.cs
@@ -59,10 +59,11 @@ namespace AssignmentTwo
 
     class Program
     {
+        static List<ClassMate> listOfClassMates = new List<ClassMate>();
+
         static void Main(string[] args)
         {
             LogIn();
-            Menu();
             Console.ReadKey();
         }
 
@@ -84,12 +85,13 @@ namespace AssignmentTwo
                     Console.WriteLine("Wrong password, try again:");
                 }
             } while (wrongInput);
-            
+            CreateMembers();
+            Menu();
         }
         private static void Menu()
         {
-            CreateMembers();
             string input = "";
+            Console.WriteLine();
             Console.WriteLine("Menu");
             Console.WriteLine("1. List all members");
             Console.WriteLine("2. Ten details");
@@ -126,24 +128,39 @@ namespace AssignmentTwo
 
         private static void ListAllMembers()
         {
-            foreach (var member in classMates)
+            foreach (var classMate in listOfClassMates)
             {
-
+                Console.Write($"{classMate.Name},");
             }
+            Menu();
         }
         private static void DisplayMemberDetails()
         {
-
-        }
+            foreach (var classMate in listOfClassMates)
+            {
+                Console.Write($"Name: {classMate.Name} Age: {classMate.Age} Length: {classMate.Length} Adress: {classMate.Adress} Hobby: {classMate.Hobby} Favorite Food: {classMate.FavFood} Favorite Drink: {classMate.FavDrink} Favorite Band: {classMate.FavBand} Children: {classMate.NrChildren}");
+            }
+        }6
         private static void DisplayMembersDriveToProgramming()
         {
-
+            foreach (var classMate in listOfClassMates)
+            {
+               Console.WriteLine($"{classMate.Name}, Programmerings driv: {classMate.ProgrammingDrive}");
+            }
+            Menu();
         }
         private static void RemoveMember()
         {
-
+            string input = "";
+            Console.WriteLine("Who do you wanna remove?");
+            for (int i = 0; i < listOfClassMates.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {listOfClassMates[i].Name}");
+            }
+            input = Console.ReadLine();
+            Menu();
         }
-        public static void CreateMembers()
+        static void CreateMembers()
         {
             ClassMate DennisLindquist = new ClassMate("Dennis Lindquist", 32, 182, "Älvdalen", "Musik/Gitarr", "Friterad kyckling", "Öl", "Metallica", 1, "Att får skapa och kunna vara kreativ. Men även att få göra ett byte av karriär.");
             ClassMate FredrikHoffman = new ClassMate("Fredrik Hoffman", 28, 192, "Odensala", "Musik, socialisera, film", "Entrecote med rotfrukter och vitlökssmör", "Trocadero Zero", "Armin Van Buuren", 0, "Möjlighet till karriärutveckling");
@@ -155,8 +172,18 @@ namespace AssignmentTwo
             ClassMate MattiasVikstrom = new ClassMate("Mattias Vikström", 33, 187, "Umeå", "Fiske, matlagning", "Cowboysoppa", "Gin och Tonic", "Infected Mushroom", 0, "Personlig utveckling och kreativitet.");
             ClassMate NicklasEriksson = new ClassMate("Nicklas Eriksson", 26, 175, "Umeå", "Skidor, cykel, simma, springa, fjällvandring, klättring och dataspel", "Gröt med jordnötssmör", "Whiskey", "Falling in Reverse och Self Deception", 0, "Drivet kommer från att man får vara kreativ och en problemlösare på samma gång. Sen så drivs man såklart av att få testa på en annan karriär än den man har haft tidigare.");
             ClassMate JosefineRonnqvist = new ClassMate("Josefine Rönnqvist", 34, 164, "Gideå", "Sy, pussla, umgås", "Frukt", "Vatten", "Halsbandet", 2, "Personlig utveckling och karriärbyte.");
+            listOfClassMates.Add(DennisLindquist);
+            listOfClassMates.Add(FredrikHoffman);
+            listOfClassMates.Add(BennyChristensen);
+            listOfClassMates.Add(HakanEriksson);
+            listOfClassMates.Add(TinaEriksson);
+            listOfClassMates.Add(TobiasBinett);
+            listOfClassMates.Add(EmilOrjes);
+            listOfClassMates.Add(MattiasVikstrom);
+            listOfClassMates.Add(NicklasEriksson);
+            listOfClassMates.Add(JosefineRonnqvist);
 
-            List<ClassMate> classMates = new List<ClassMate>() { DennisLindquist, FredrikHoffman, BennyChristensen, HakanEriksson, TinaEriksson, TobiasBinett, EmilOrjes, MattiasVikstrom, NicklasEriksson, JosefineRonnqvist };
+
         }
     }
 }
